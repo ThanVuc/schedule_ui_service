55d63ca8909da4f71eed627942b268176987b390
huan <thaidaihuan@gmail.com>
2025-06-29 18:05:40 +0700

    [Users] fix layout and add page

--- src/app/(pages)/(main)/_components/SidebarO.tsx
+++ src/app/(pages)/(main)/_components/SidebarO.tsx
@@ -3,7 +3,6 @@ import ItemsSideBar from "./item-side-bar";
 const SideBar = () => {
     return (
         <div className="h-full w-full border-r border-gray-400 flex flex-col">
-
             <ItemsSideBar />
         </div>
     );
